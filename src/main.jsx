f90b7b1cde539a535901e609a6b00932414b4519
jarredkeam <163301951+jarredkeam@users.noreply.github.com>
2026-09-02 01:36:12 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <h1>AgriLine SA USSD Simulator</h1>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
